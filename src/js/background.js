a1d25de9f5455f9e8038df58a88f1a69b56582ce
fredric Lundberg <Diriko1997@live.com>
2021-04-29 14:03:45 +0200

    add vocabulary

--- src/js/background.js
+++ src/js/background.js
@@ -1,8 +1,26 @@
 import '../img/icon-128.png'
 import '../img/icon-34.png'
 import * as tf from '@tensorflow/tfjs';
-const fetch = require('node-fetch');
 var model;
+
+console.log('from background js')
+
+
+async function fetchVocabulary() {
+    console.log("in getVocabulary");
+    let url = 'https://raw.githubusercontent.com/Hydrogen-Spoiler-Blocker/models/master/vocabulary.txt';
+    console.log("fetching url...")
+    let response = await fetch(url);
+    // get one header
+    console.log(response.headers.get('Content-Type'));
+    let body = await response.text(); 
+    let vocab_array = body.split("\n")
+    console.log(body[13])
+    console.log(vocab_array);
+    console.log("vocab fetched") 
+    return vocab_array
+}
+
 
 async function loadModel() {
     
@@ -11,10 +29,35 @@ async function loadModel() {
     console.log("model loaded")
 }
 
-console.log('from background')
-loadModel();
 
-function makePrediction() {
+const encoder = (string, vocabulary) => {
+    let array = string.toLowerCase().split(" ")
+    let vectorized = []
+    for (const element of array) {
+      let position = vocabulary.indexOf(element)
+      if (position === -1) {
+        vectorized.push(1)
+      } else {
+        vectorized.push(position)
+      }
+    }
+    return vectorized
+}
+
+loadModel().then(() => {
+    fetchVocabulary().then(vocab => {
+        const text = "movie is amazing movie is a cool kid" //8 st
+        console.log("parsed text from vocabulary: ",encoder(text, vocab))
+        var vocablength = encoder(text, vocab).length
+        output = model.predict(tf.tensor2d([ encoder(text, vocab)]))
+        //output = model.predict([encoder(text, vocab), [vocablength, 1]])
+        console.log("PREDICTION : ", output );
+
+    });
+})
+
+
+function makePrediction(paragraph) {
     var a, b, output;
     a = 0
     b = 1
@@ -26,16 +69,38 @@ function makePrediction() {
     console.log("predicted: ", outputData[0]);
     console.log(Number(outputData[0] > 0.5))
     console.log("predicted^")
+    return Number(outputData[0] > 0.5)
     //document.getElementById("answer").value = Number(outputData[0] > 0.5);
 }
 
+window.addEventListener('load', (event) => {
+    console.log("Loaded load loady lod");
+})
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if(changeInfo.status == 'complete') {
+        console.log("ON UPDAAATED");
+    }
+})
+
+
 chrome.tabs.onActivated.addListener(tab => {
     chrome.tabs.get(tab.tabId, current_tab => {
-        console.log(current_tab.url);
+        console.log("current URL: ",current_tab.url);
         if(/^https:/.test(current_tab.url)){
 
+            var paragraphs = document.getElementsByTagName("p")
+            console.log("dennis: ", paragraphs);
             
-            makePrediction()
+
+            /*
+                webscraper.forEach(text){
+                    if(makeprediction(text) > 0.5){
+                        //block text
+                    }
+                }
+            */ 
+            makePrediction("hello there")
             console.log("We are inside tabs onActivated")
             
         }
